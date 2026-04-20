7ecc34d97398dfdc261f4dec5c1e1f164e75bc9e
ArthurLacerdaa <arthurdl2005@gmail.com>
2026-04-20 14:37:10 -0300

    Atualizando urls e views

--- backend/tarefas/urls.py
+++ backend/tarefas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import listar_tarefa
-from .views import abertas
+from .views import listar_tarefa, tarefas_por_status_prioridade, tarefa_por_id, atrasada , buscar_titulo
 
 urlpatterns = [
     path('tarefas/' , listar_tarefa),
-    path('tarefas/abertas' , abertas)
+    path('tarefas/filtro/<str:status>/<str:prioridade>/', tarefas_por_status_prioridade),
+    path('tarefas/id/<int:id>/' , tarefa_por_id),
+    path('tarefas/busca/<str:titulo>/', buscar_titulo),
+    path('tarefas/atrasadas/', atrasada)
 ]
 
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,10 +1,37 @@
 from django.http import JsonResponse
 from .models import Tarefa
+from django.utils import timezone
 
 def listar_tarefa(request):
     tarefas = Tarefa.objects.all().values()
     return JsonResponse(list(tarefas) , safe=False)
 
-def abertas(request):
-    tarefa_aberta = Tarefa.objects.filter(status='ABERTA').values()
-    return JsonResponse(list(tarefa_aberta) , safe=False)
+from django.http import JsonResponse
+from .models import Tarefa
+
+def tarefas_por_status_prioridade(request, status, prioridade):
+    tarefas = Tarefa.objects.filter(status=status)
+
+    if prioridade:
+        tarefas = tarefas.filter(prioridade=prioridade)
+    
+    data = list(tarefas.values())
+    return JsonResponse(data, safe=False)
+
+def tarefa_por_id(request, id):
+    tarefa = Tarefa.objects.filter(id=id).values().first()
+
+    if tarefa:
+        return JsonResponse(tarefa)
+    return JsonResponse({'erro': 'Tarefa nao encontrada'})
+    
+def atrasada(request):
+
+    hoje = timezone.now().date()
+
+    tarefa_atrasada = Tarefa.objects.filter(data_entrega__lt = hoje, status = 'ABERTA').values()
+    return JsonResponse(list(tarefa_atrasada), safe=False)
+
+def buscar_titulo(request, titulo):
+    tarefa_titulo = Tarefa.objects.filter(titulo__iexact = titulo).values()
+    return JsonResponse(list(tarefa_titulo), safe=False)
