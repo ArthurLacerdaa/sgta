3f7b4665678e4caaa6d8083e782d1d3d0c7d4429
ArthurLacerdaa <arthurdl2005@gmail.com>
2026-05-26 18:14:14 -0300

    Resolvendo conflitos

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -22,17 +22,11 @@ class Tarefa(models.Model):
     
 
 
-    # Lista de valores válidos para o campo 'prioridade'.
-    PRIORIDADE_CHOICES = [
-        ('URGENTE', 'Urgente'),
-        ('NAO_URGENTE', 'Não urgente'),
-    ]
-
     # Título da tarefa — texto curto, limitado a 255 caracteres.
     titulo = models.CharField(max_length=255)
 
     descricao = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='ABERTA')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='ABERTA')
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_entrega = models.DateField()
     prioridade = models.CharField(max_length=40 , choices=PRIORIDADE_CHOICES, default='NAO_URGENTE')
